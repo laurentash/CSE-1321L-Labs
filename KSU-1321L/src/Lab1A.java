48a9b1652f2d2c52dfc3b6a662709cac265aa24d
Lauren Shockley <laurenshockley130@icloud.com>
2024-01-19 17:07:52 -0500

    I can't believe it took so long to fix this.

--- KSU-1321L/src/Lab1A.java
+++ KSU-1321L/src/Lab1A.java
@@ -1,9 +1,10 @@
-public class Lab1A {
+public class Lab1A
+{
 
     public static void main(String[] args)
     {
 
-System.out.println("heeeeyyyyyyy");
+        System.out.println("something anything please");
 
     }
 
